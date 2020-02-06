c308ac69163c58e716be5d877f0e4fb4ffd8fb5e
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-02-06 13:11:20 -0200

    Adds base Python project
    - Creates a Python package and a setup.py
    - Adds a tests folder for unit tests
    - Adds a .gitignore for Python projects
    - README.md explains the installation and testing process

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+extras = {
+    "testing": [
+        "pytest>=4.4.0",
+        "pytest-xdist==1.31.0",
+        "pytest-cov==2.8.1",
+        "codecov==2.0.15",
+    ]
+}
+
+setup(
+    name="platiagro",
+    version="0.0.1",
+    author="Fabio Beranizo Lopes",
+    author_email="fabio.beranizo@gmail.com",
+    description="Python SDK for PlatIAgro.",
+    license="Apache",
+    url="https://github.com/platiagro/sdk",
+    packages=[],
+    install_requires=[
+    ],
+    extras_require=extras,
+    python_requires=">=3.5.0",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
