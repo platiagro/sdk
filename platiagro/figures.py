64a2e77af0db800ccb9b1ad8cca1701017c05e08
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-03-16 11:25:23 -0300

    List figures using data URI scheme
    Example:
    data:image/png;base64,....

--- platiagro/figures.py
+++ platiagro/figures.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from base64 import b64encode
 from io import BytesIO
+from os import SEEK_SET
 from os.path import join
 from tempfile import _get_candidate_names
 from typing import List
@@ -12,13 +14,13 @@ PREFIX = "experiments"
 
 
 def list_figures(experiment_id: str) -> List[str]:
-    """Lists all figures from object storage.
+    """Lists all figures from object storage as data URI scheme.
 
     Args
         experiment_id (str): the experiment name.
 
     Returns:
-        A list of all figures names.
+        A list of data URIs.
     """
     figures = []
 
@@ -28,8 +30,16 @@ def list_figures(experiment_id: str) -> List[str]:
     prefix = join(PREFIX, experiment_id, "figure-")
     objects = MINIO_CLIENT.list_objects_v2(BUCKET_NAME, prefix)
     for obj in objects:
-        name = obj.object_name[len(PREFIX) + 1:]
-        figures.append(name)
+        data = MINIO_CLIENT.get_object(
+            bucket_name=BUCKET_NAME,
+            object_name=obj.object_name,
+        )
+        buffer = b""
+        for d in data.stream(32*1024):
+            buffer += d
+        encoded_figure = b64encode(buffer).decode("utf8")
+        figure = "data:image/png;base64,{}".format(encoded_figure)
+        figures.append(figure)
     return figures
 
 
