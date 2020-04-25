8dcd4d0ddb9ac56c5a6026dee4b29f831768a953
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-04-25 13:52:05 -0300

    Adds __version__ to root package
    Uses this version in setup.py.
    Anyone may also check the version by using projects.__version__

--- platiagro/__init__.py
+++ platiagro/__init__.py
@@ -1,5 +1,15 @@
-from .datasets import *
-from .models import *
-from .featuretypes import *
-from .metrics import *
-from .figures import *
+from .datasets import list_datasets, load_dataset, save_dataset, stat_dataset
+from .featuretypes import infer_featuretypes, validate_featuretypes, \
+    DATETIME, CATEGORICAL, NUMERICAL
+from .figures import list_figures, save_figure
+from .metrics import save_metrics
+from .models import load_model, save_model, stat_model
+
+__all__ = ["list_datasets", "load_dataset", "save_dataset", "stat_dataset",
+           "infer_featuretypes", "validate_featuretypes",
+           "DATETIME", "CATEGORICAL", "NUMERICAL",
+           "list_figures", "save_figure",
+           "save_metrics",
+           "load_model", "save_model", "stat_model", ]
+
+__version__ = "0.0.2"
