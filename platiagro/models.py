65bf8fff9a751c34e70f75f5746df2d8cf516294
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-06-01 16:51:59 -0300

    Concatenates operator_id to model path
    Bug fix: subsequent components overwrite a model saved previously.
    Updates unit tests.

--- platiagro/models.py
+++ platiagro/models.py
@@ -6,32 +6,43 @@ from typing import Dict, Optional
 from joblib import dump, load
 from minio.error import NoSuchBucket, NoSuchKey
 
-from .util import BUCKET_NAME, MINIO_CLIENT, get_experiment_id, make_bucket
+from .util import BUCKET_NAME, MINIO_CLIENT, get_experiment_id, \
+    get_operator_id, make_bucket
 
-PREFIX = "experiments"
+PREFIX_1 = "experiments"
+PREFIX_2 = "operators"
 MODEL_FILE = "model.joblib"
 
 
-def load_model(experiment_id: Optional[str] = None) -> Dict[str, object]:
-    """Retrieves a model.
+def load_model(experiment_id: Optional[str] = None,
+               operator_id: Optional[str] = None) -> Dict[str, object]:
+    """Retrieves a model from object storage.
 
     Args:
         experiment_id (str, optional): the experiment uuid. Defaults to None.
+        operator_id (str, optional): the operator uuid. Defaults to None.
 
     Returns:
         dict: A dictionary of models.
+
+    Raises:
+        TypeError: when experiment_id is undefined in args and env.
+        TypeError: when operator_id is undefined in args and env.
     """
     if experiment_id is None:
         experiment_id = get_experiment_id()
 
+    if operator_id is None:
+        operator_id = get_operator_id()
+
     try:
-        object_name = f'{PREFIX}/{experiment_id}/{MODEL_FILE}'
+        object_name = f"{PREFIX_1}/{experiment_id}/{PREFIX_2}/{operator_id}/{MODEL_FILE}"
         data = MINIO_CLIENT.get_object(
             bucket_name=BUCKET_NAME,
             object_name=object_name,
         )
     except (NoSuchBucket, NoSuchKey):
-        raise FileNotFoundError(f"No such file or directory: '{experiment_id}'")
+        return {}
 
     buffer = BytesIO(data.read())
 
@@ -42,14 +53,20 @@ def save_model(**kwargs):
     """Serializes and saves models.
 
     Args:
-        experiment_id (str, optional): the experiment uuid. Defaults to None.
         **kwargs: the models as keyword arguments.
+
+    Raises:
+        TypeError: when a figure is not a matplotlib figure.
     """
     experiment_id = kwargs.get("experiment_id")
     if experiment_id is None:
         experiment_id = get_experiment_id()
 
-    object_name = f'{PREFIX}/{experiment_id}/{MODEL_FILE}'
+    operator_id = kwargs.get("operator_id")
+    if operator_id is None:
+        operator_id = get_operator_id()
+
+    object_name = f"{PREFIX_1}/{experiment_id}/{PREFIX_2}/{operator_id}/{MODEL_FILE}"
 
     model_buffer = BytesIO()
     dump(kwargs, model_buffer)
