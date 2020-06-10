2eb6b715391740fb6172139b5e870fab47f25c25
Ricardo Shiguemi Hiramatsu <38075762+shiguemi32@users.noreply.github.com>
2020-06-10 19:47:40 -0300

    Check if contract is a valid json (#32)
    * Check if contract is a valid json
    
    * Sets max-line-length = 127 in tox.ini
    
    The GitHub editor is 127 chars wide.
    
    * Check invalid contract and invalid json
    
    * Remove unused import
    
    * Tensor to false
    
    Co-authored-by: Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
    Co-authored-by: Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
    Co-authored-by: shiguemi32 <ricardoh@AA150632.aquarius.cpqd.com.br>

--- platiagro/deployment.py
+++ platiagro/deployment.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """A module for testing components before deployment."""
-from os import environ
+from os import environ, kill
 from random import randint
 from sys import stderr
 from subprocess import PIPE, Popen
@@ -10,9 +10,13 @@ from requests import Session
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
 from requests.packages.urllib3.util.retry import Retry
+from seldon_core.microservice_tester import run_method
 
 from .util import get_experiment_id, get_operator_id
 
+class Bunch(object):
+    def __init__(self, adict):
+        self.__dict__.update(adict)
 
 def test_deployment(contract: str,
                     module: str = "Model",
@@ -82,20 +86,20 @@ def test_deployment(contract: str,
             print(pserver.stderr.read().decode(), file=stderr, flush=True)
             return
 
-        cmd = (
-            f"seldon-core-microservice-tester "
-            f"{contract} "
-            f"localhost "
-            f"{port} "
-            f"--endpoint "
-            f"predict "
-            f"-p"
-        )
-
-        # start a process to test the server
-        with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE) as pclient:
-            print(pclient.stdout.read().decode(), flush=True)
-
-        # kill HTTP server
-        pserver.kill()
-        print(pserver.stderr.read().decode(), flush=True)
+        try:
+            args_dict = {
+                "contract": contract,
+                "host": "localhost",
+                "port": port,
+                "n_requests": 1,
+                "batch_size": 1,
+                "grpc": False,
+                "tensor": False,
+                "prnt": True
+            }
+            args = Bunch(args_dict)
+            run_method(args, "predict")
+        finally:
+            # kill HTTP server
+            kill(pserver.pid, 9)
+            print(pserver.stderr.read().decode(), flush=True)
--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from json import dump
+from json.decoder import JSONDecodeError
 from os import remove
 from unittest import TestCase
+
+from seldon_core.microservice_tester import SeldonTesterException
 
 from platiagro import deployment
 
@@ -65,9 +68,55 @@ class TestDeployment(TestCase):
                 ]
             }
             dump(contract, f)
+        
+        with open("invalid_contract.json", "w") as f:
+            invalid_contract = {
+                "features":[
+                    {
+                        "name":"sepal_length",
+                        "dtype":"FLOAT",
+                        "ftype":"FOO",
+                        "range":[4,8]
+                    },
+                    {
+                        "name":"sepal_width",
+                        "dtype":"FLOAT",
+                        "ftype":"continuous",
+                        "range":[2,5]
+                    },
+                    {
+                        "name":"petal_length",
+                        "dtype":"FLOAT",
+                        "ftype":"continuous",
+                        "range":[1,10]
+                    },
+                    {
+                        "name":"petal_width",
+                        "dtype":"FLOAT",
+                        "ftype":"continuous",
+                        "range":[0,3]
+                    }
+                ],
+                "targets":[
+                    {
+                        "name":"class",
+                        "dtype":"FLOAT",
+                        "ftype":"continuous",
+                        "range":[0,1],
+                        "repeat":3
+                    }
+                ]
+            }
+            dump(invalid_contract, f)
+
+        with open("invalid_json.json", "w") as f:
+            f.write("""{"features": [a]}""")
+            f.close()
 
     def tearDown(self):
         remove("contract.json")
+        remove("invalid_contract.json")
+        remove("invalid_json.json")
 
     def test_test_deployment_success(self):
         with open("Model.py", "w") as f:
@@ -94,7 +143,6 @@ class TestDeployment(TestCase):
         remove("Model.py")
 
     def test_test_deployment_error_on_start(self):
-
         with open("Model.py", "w") as f:
             f.write((
                 f"import logging\n"
@@ -143,3 +191,51 @@ class TestDeployment(TestCase):
             ))
         deployment.test_deployment("contract.json")
         remove("Model.py")
+
+    def test_test_deployment_invalid_contract(self):
+        with self.assertRaises(SeldonTesterException):
+            with open("Model.py", "w") as f:
+                f.write((
+                    f"import logging\n"
+                    f"from typing import List, Iterable, Dict, Union\n"
+                    f"\n"
+                    f"import numpy as np\n"
+                    f"\n"
+                    f"logger = logging.getLogger(__name__)\n"
+                    f"\n"
+                    f"\n"
+                    f"class Model(object):\n"
+                    f"    def __init__(self, dataset: str = None, target: str = None):\n"
+                    f"        self.columns_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']\n"
+                    f"\n"
+                    f"    def class_names(self):\n"
+                    f"        return self.columns_names\n"
+                    f"\n"
+                    f"    def predict(self, X: np.ndarray, feature_names: Iterable[str], meta: Dict = None) -> Union[np.ndarray, List, str, bytes]:\n"
+                    f"        return X\n"
+                ))
+            deployment.test_deployment("invalid_contract.json")
+
+    def test_test_deployment_invalid_json(self):
+        with self.assertRaises(JSONDecodeError):
+            with open("Model.py", "w") as f:
+                f.write((
+                    f"import logging\n"
+                    f"from typing import List, Iterable, Dict, Union\n"
+                    f"\n"
+                    f"import numpy as np\n"
+                    f"\n"
+                    f"logger = logging.getLogger(__name__)\n"
+                    f"\n"
+                    f"\n"
+                    f"class Model(object):\n"
+                    f"    def __init__(self, dataset: str = None, target: str = None):\n"
+                    f"        self.columns_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']\n"
+                    f"\n"
+                    f"    def class_names(self):\n"
+                    f"        return self.columns_names\n"
+                    f"\n"
+                    f"    def predict(self, X: np.ndarray, feature_names: Iterable[str], meta: Dict = None) -> Union[np.ndarray, List, str, bytes]:\n"
+                    f"        return X\n"
+                ))
+            deployment.test_deployment("invalid_json.json")
