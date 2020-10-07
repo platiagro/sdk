8616eabad001e42b96915ee71c224cceb551e287
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2020-10-07 15:54:04 -0300

    Save metrics per run_id

--- platiagro/metrics.py
+++ platiagro/metrics.py
@@ -11,25 +11,22 @@ from minio.error import NoSuchBucket, NoSuchKey
 
 from .figures import save_figure
 from .util import BUCKET_NAME, MINIO_CLIENT, get_experiment_id, \
-    get_operator_id, make_bucket
+    get_operator_id, make_bucket, get_run_id, stat_metadata, operator_filepath
 
-PREFIX_1 = "experiments"
-PREFIX_2 = "operators"
 METRICS_FILE = "metrics.json"
 CONFUSION_MATRIX = "confusion_matrix"
 
 
 def list_metrics(experiment_id: Optional[str] = None,
-                 operator_id: Optional[str] = None) -> List[Dict[str, object]]:
+                 operator_id: Optional[str] = None,
+                 run_id: Optional[str] = None) -> List[Dict[str, object]]:
     """Lists metrics from object storage.
-
     Args:
         experiment_id (str, optional): the experiment uuid. Defaults to None.
         operator_id (str, optional): the operator uuid. Defaults to None.
-
+        run_id (str, optional): the run id. Defaults to None.
     Returns:
         list: A list of metrics.
-
     Raises:
         TypeError: when experiment_id is undefined in args and env.
         TypeError: when operator_id is undefined in args and env.
@@ -40,8 +37,22 @@ def list_metrics(experiment_id: Optional[str] = None,
     if operator_id is None:
         operator_id = get_operator_id()
 
+    if run_id is None:
+        # gets run_id from env variable
+        # Attention: returns None if env is unset
+        run_id = get_run_id()
+    elif run_id == "latest":
+        try:
+            metadata = stat_metadata(experiment_id, operator_id)
+            run_id = metadata.get("run_id")
+        except FileNotFoundError:
+            return []
+
+    # ensures MinIO bucket exists
+    make_bucket(BUCKET_NAME)
+
     try:
-        object_name = f"{PREFIX_1}/{experiment_id}/{PREFIX_2}/{operator_id}/{METRICS_FILE}"
+        object_name = operator_filepath(METRICS_FILE, experiment_id, operator_id, run_id)
         data = MINIO_CLIENT.get_object(
             bucket_name=BUCKET_NAME,
             object_name=object_name,
@@ -52,18 +63,16 @@ def list_metrics(experiment_id: Optional[str] = None,
     return load(data)
 
 
-def save_metrics(reset: bool = False,
-                 experiment_id: Optional[str] = None,
+def save_metrics(experiment_id: Optional[str] = None,
                  operator_id: Optional[str] = None,
+                 run_id: Optional[str] = None,
                  **kwargs):
     """Saves metrics of an experiment to the object storage.
-
     Args:
-        reset (bool): whether to reset the metrics. Defaults to False.
         experiment_id (str, optional): the experiment uuid. Defaults to None
         operator_id (str, optional): the operator uuid. Defaults to None
+        run_id (str, optional): the run id. Defaults to None.
         **kwargs: the metrics dict.
-
     Raises:
         TypeError: when experiment_id is undefined in args and env.
         TypeError: when operator_id is undefined in args and env.
@@ -74,23 +83,46 @@ def save_metrics(reset: bool = False,
     if operator_id is None:
         operator_id = get_operator_id()
 
-    object_name = f"{PREFIX_1}/{experiment_id}/{PREFIX_2}/{operator_id}/{METRICS_FILE}"
+    if run_id is None:
+        # gets run_id from env variables
+        # Attention: returns None if env is unset
+        run_id = get_run_id()
+
+    if run_id:
+        metadata = {}
+        try:
+            metadata = stat_metadata(experiment_id, operator_id)
+            if run_id == "latest":
+                run_id = metadata.get("run_id")
+        except FileNotFoundError:
+            pass
+        metadata["run_id"] = run_id
+
+        # encodes metadata to JSON format and uploads to MinIO
+        buffer = BytesIO(dumps(metadata).encode())
+        MINIO_CLIENT.put_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f'experiments/{experiment_id}/operators/{operator_id}/.metadata',
+            data=buffer,
+            length=buffer.getbuffer().nbytes,
+        )
 
     # ensures MinIO bucket exists
     make_bucket(BUCKET_NAME)
 
+    object_name = operator_filepath(METRICS_FILE, experiment_id, operator_id, run_id)
+
     encoded_metrics = []
 
     # retrieves the metrics saved previosuly
-    if not reset:
-        try:
-            data = MINIO_CLIENT.get_object(
-                bucket_name=BUCKET_NAME,
-                object_name=object_name,
-            )
-            encoded_metrics = loads(data.read())
-        except NoSuchKey:
-            pass
+    try:
+        data = MINIO_CLIENT.get_object(
+            bucket_name=BUCKET_NAME,
+            object_name=object_name,
+        )
+        encoded_metrics = loads(data.read())
+    except NoSuchKey:
+        pass
 
     # appends new metrics
     encoded_metrics.extend(encode_metrics(kwargs))
@@ -108,10 +140,6 @@ def save_metrics(reset: bool = False,
 
     # makes plots for some metrics
     if CONFUSION_MATRIX in kwargs:
-
-        if operator_id is None:
-            operator_id = get_operator_id()
-
         confusion_matrix = kwargs[CONFUSION_MATRIX]
         plt.clf()
         plot = plot_confusion_matrix(confusion_matrix)
