c1013b80140109d19eb30e25879e2357a4706084
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2021-02-11 11:45:20 -0300

    Removes tornado from requirements
    Since platiagro.deployments was removed, it's unnecessary to pin
    tornado version.

--- setup.py
+++ setup.py
@@ -16,10 +16,9 @@ with open("requirements.txt") as f:
 
 extras = {
     "plotting": [
-        "shap==0.37.0",
+        "shap>=0.37.0",
         "seaborn>=0.10.0",
         "plotly>=4.14.1",
-        "tornado>=4.3,<6",
         "ipykernel>=5.2.0",
         "notebook>=6.0.2",
     ],
