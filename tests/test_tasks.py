30cf1f8adf8aa4787917e7e357f19101c6f3636e
dnlcesilva <dnlcesilva@gmail.com>
2021-05-11 13:55:38 -0300

    Creation of API to create task.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,10 +1,11 @@
+from os import name
 from unittest import TestCase
 from platiagro.tasks import create_task
 
 
 class TestTasks(TestCase):
     def test_create_tasks(self):
-        response = create_task()
+        response = create_task(name="TesteTasks")
         self.assertIsInstance(response, dict)
 
 
