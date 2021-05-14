880ebb96bc9f4d89d4e8e472ecd1d010cdb4cdd6
Danilo Carvalho e Silva <43361324+dnlcesilva@users.noreply.github.com>
2021-05-14 18:09:11 -0300

    Add files via upload

---
+++ platiagro/deployments.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import requests
+from platiagro.util import PROJECTS_ENDPOINT
+
+
+def list_projects():
+    """Lists the projects.
+
+    Returns:
+        requests.Response: the response returned list Projects.
+    """
+    response = requests.get(url=f"{PROJECTS_ENDPOINT}/projects")
+    return response.json()
+
+
+def get_project_name(project_name: str):
+    """Lists projects by name.
+
+    Args:
+        project_name (str): the project name.
+
+    Returns:
+        list: returns the list of projects.
+    """
+    projects = list_projects()
+
+    for project in projects["projects"]:
+        if project["name"] == project_name:
+            return project
+
+
+def list_deployments(project_name: str):
+    """Lists the deployments.
+
+    Args:
+        project_name (str): the project name.
+
+    Returns:
+        requests.Response: returns the list of deployments.
+    """
+    project = get_project_name(project_name)
+    project_id = project["uuid"]
+    response = requests.get(url=f"{PROJECTS_ENDPOINT}/projects/{project_id}/deployments")
+    return response.json()
+
+
+def get_deployment_name(deployment_name: str, project_name: str):
+    """Lists deployments by name.
+
+    Args:
+        deployment_name (str): the deployment name.
+        project_name (str): the project name.
+
+    Returns:
+        list: returns the list of deployments by name.
+    """
+    deployments = list_deployments(project_name)
+    for deployment in deployments["deployments"]:
+        if deployment["name"] == deployment_name:
+            return deployment
+
+
+def run_deployments(deployment_name: str, project_name: str):
+    """Runs a deployement.
+
+    Args:
+        deployment_name (str): the deployment name.
+        project_name (str): the project name.
+
+    Returns:
+        requests.Response: a dictionary with information about the deployment runs.
+    """
+    project = get_project_name(project_name)
+    deployment = get_deployment_name(deployment_name, project_name)
+    project_id = project["uuid"]
+    deployment_id = deployment["uuid"]
+    response = requests.post(url=f"{PROJECTS_ENDPOINT}/projects/{project_id}/deployments/{deployment_id}/runs")
+    return response.json()
