b0df0ef01ecb81c18075ab865166a90154ddf052
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2021-05-19 10:52:38 -0300

    Fix test_deployments

--- tests/client/test_deployments.py
+++ tests/client/test_deployments.py
@@ -46,47 +46,4 @@ class TestRuns(TestCase):
         self.assertEqual(result["name"], DEPLOYMENT_NAME)
 
     # TODO: Fix this test.
-    @mock.patch("platiagro.deployments.requests.post")
-    @mock.patch("platiagro.deployments.get_project_by_name")
-    @mock.patch("platiagro.deployments.get_deployment_by_name")
-    def test_run_deployments(self, mock_post, mock_get_project, mock_get_deployments):
-        mock_response = mock.Mock(
-            status_code=200
-            )
-        mock_get_project.return_value = {
-            "uuid": "bc4a0874-4a6b-4e20-bd7e-ed00c51fd8ea",
-            "name": "projects01"
-        }
-
-        mock_get_deployments.return_value = {
-            "uuid": "c1406cc2-f82e-4d97-b82a-274880b2ce2d",
-            "name": "deployments01"
-        }
-
-        mock_post.json.return_value = {
-            "uuid": "f72a180a-2b8f-4a16-b2c6-e93a27ff8da0",
-            "operators": {
-                "a4a16d55-f745-4e06-9273-d26c16b20269": {
-                    "status": "Pending",
-                    "parameters": {
-                        "dataset": "null",
-                        "features_to_filter": [
-                            "nox"
-                        ]
-                    },
-                    "taskId": "c03dfde0-e6bb-4e83-a370-8caa99c79cff"
-                },
-                "deployment": {
-                    "status": "Pending",
-                    "parameters": {}
-                }
-            },
-            "createdAt": "2021-05-18T00:21:16+00:00",
-            "deploymentId": "c2ac8d9b-8a0a-4a41-ac8b-48a5685f6c86"
-        }
-        mock_post.return_value = mock_response
-        # Estamos com dificuldade para mockar o teste run_deployments, estamos procurando uma alternativa
-        # result = run_deployments("projects01", "deployments01")
-
-        # self.assertTrue(isinstance(result, dict))
-        # self.assertEqual(result.status_code, 200)
+    # Estamos com dificuldade para mockar o teste run_deployments, estamos procurando uma alternativa
