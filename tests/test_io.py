07843857bc307b81395611494d889cc51707d326
Fabio Beranizo <fabiol@cpqd.com.br>
2021-09-08 20:24:33 -0300

    Refactors test_io.py
    Adds docstrings and formats file using black.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,15 +1,26 @@
+# -*- coding: utf-8 -*-
+import unittest
+import unittest.mock as mock
+
 import platiagro.io
 
 
-class TestIO:
+class TestIO(unittest.TestCase):
+    @mock.patch("platiagro.io.zipfile")
+    def test_unzip_file(self, mock_zipfile):
+        """
+        Should extract zipfile to given path.
+        """
+        path_to_zip_file = "/path/to.zip"
+        directory_to_extract_to = "/path/to/extract"
 
-    def test_unzip_file(self, mocker):
-        mock_zipfile = mocker.MagicMock(name="zipfile")
-        mocker.patch("platiagro.io.zipfile", new=mock_zipfile)
+        platiagro.io.unzip_to_folder(
+            path_to_zip_file=path_to_zip_file,
+            directory_to_extract_to=directory_to_extract_to,
+        )
 
-        platiagro.io.unzip_to_folder("/path/to.zip", "/path/to/extract")
-
-        mock_zipfile.ZipFile.assert_called_once_with("/path/to.zip", "r")
+        mock_zipfile.ZipFile.assert_called_once_with(path_to_zip_file, "r")
         mock_zipfile.ZipFile.return_value.__enter__.assert_called_once_with()
-        mock_zipfile.ZipFile.return_value.__enter__.return_value.extractall. \
-            assert_called_once_with("/path/to/extract")
+        mock_zipfile.ZipFile.return_value.__enter__.return_value.extractall.assert_called_once_with(
+            "/path/to/extract"
+        )
