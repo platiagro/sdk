42f62ce210a6ce3fe961af25497d72d692677252
Fabio Beranizo <fabiol@cpqd.com.br>
2021-09-08 20:24:02 -0300

    Formats platiagro/io.py using black
    Also adds missing docstring.

--- platiagro/io.py
+++ platiagro/io.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
 import zipfile
 
 
-def unzip_to_folder(path_to_zip_file, directory_to_extract_to):
-    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+def unzip_to_folder(path_to_zip_file: str, directory_to_extract_to: str):
+    """Unzips .zip file into a given directory.
+
+    Args:
+        path_to_zip_file (str): path to zipfile.
+        directory_to_extract_to (str): destination path.
+    """
+    with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
